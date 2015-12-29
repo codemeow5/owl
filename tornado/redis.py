9914b17ee579aaf59531dce7d79f7f3038955556
Codemeow <codemeow@yahoo.com>
2015-12-29 18:04:15 +0800

    Do what comes naturally.  Seethe and fume and throw a tantrum.

--- tornado/redis.py
+++ tornado/redis.py
@@ -10,11 +10,20 @@ class R():
 	def r(cls):
 		if cls.__REDIS__ is None:
 			try:
-				cls.__REDIS__ = redis.Redis(
+				r = cls.__REDIS__ = redis.StrictRedis(
 					unix_socket_path='/var/run/redis/redis-6400.sock',
 					db=0)
+				p = cls.__PUBSUB__ = r.pubsub()
+				p.subscribe(**{'__keyevent@0__:expired': cls.__handle_expire_keep_alive})
 			except Exception, e:
 				cls.__REDIS__ = None
 		return cls.__REDIS__
 
-	
+	@classmethod
+	def __handle_expire_keep_alive(cls, message):
+		type_ = message.get('type')
+		if type_ is None or type_ == 'subscribe':
+			return
+		if type_ == 'message':
+			data = message.get('data')
+			pass
